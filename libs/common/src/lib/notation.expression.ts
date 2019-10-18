ff8d9b24cbd60816a863ca92892a776ef8b86dc2
Matthew Gregory <m.gregory@celcat.com>
2019-10-18 15:24:51 +0100

    chi notation fixed

--- libs/common/src/lib/notation.expression.ts
+++ libs/common/src/lib/notation.expression.ts
@@ -1,9 +1,10 @@
 /*
     At the time of writing (17-10-2019), the regular expression generated below was:
 
-        ^([1-9]+[msp]|[1-7]+z|([1-9]'[1-9]{2})[msp]|(?<0>[1-9])('\k<0>\k<0>|\k<0>'\k<0>|\k<0>\k<0>')[msp]|(?<1>[1-7])('\k<1>\k<1>|\k<1>'\k<1>|\k<1>\k<1>')z|
-        (?<2>[1-7])(xx\k<2>|'\k<2>\k<2>\k<2>|\k<2>'\k<2>\k<2>|\k<2>\k<2>'\k<2>|\k<2>\k<2>\k<2>'|'\k<2>'\k<2>\k<2>|\k<2>'\k<2>'\k<2>|\k<2>\k<2>'\k<2>')z|
-        (?<3>[1-9])(xx\k<3>|'\k<3>\k<3>\k<3>|\k<3>'\k<3>\k<3>|\k<3>\k<3>'\k<3>|\k<3>\k<3>\k<3>'|'\k<3>'\k<3>\k<3>|\k<3>'\k<3>'\k<3>|\k<3>\k<3>'\k<3>')[msp])+$
+        /^([1-9]+[msp]|[1-7]+z|([1-9]'[1-9]{2}|[1-9]{2}'[1-9]|[1-9]{3}')[msp]|(?<ps>[1-9])('\k<ps>{2}|\k<ps>'\k<ps>|\k<ps>{2}')[msp]
+        |(?<ph>[1-7])('\k<ph>{2}|\k<ph>'\k<ph>|\k<ph>{2}')z
+        |(?<kh>[1-7])(xx\k<kh>|'\k<kh>{3}|\k<kh>'\k<kh>{2}|\k<kh>{2}'\k<kh>|\k<kh>{3}'|'\k<kh>'\k<kh>{2}|\k<kh>'\k<kh>'\k<kh>|\k<kh>{2}'\k<kh>')z
+        |(?<ks>[1-9])(xx\k<ks>|'\k<ks>{3}|\k<ks>'\k<ks>{2}|\k<ks>{2}'\k<ks>|\k<ks>{3}'|'\k<ks>'\k<ks>{2}|\k<ks>'\k<ks>'\k<ks>|\k<ks>{2}'\k<ks>')[msp])+$/
 
     Obviously this is unmaintable, so instead of hard coded the regular expression we dynamically build it up
 */
@@ -14,7 +15,7 @@ const validRuns = [
     // concealed honors
     'H+z',
     // open chi suits - can only be from the left but doesn't check the values!
-    '(S`S{2})[msp]',
+    '(S`S{2} | S{2}`S | S{3}`)[msp]',
     // pon of suits, must be the same tile
     '(?<ps>S) (%`## | %#`# | %##`)[msp]',
     // pon of honors, must be the same tile
@@ -36,6 +37,8 @@ export const validHandExpression = new RegExp(
             .replace(/S/g, '[1-9]')
         // ...expand H to valid honor ranks
             .replace(/H/g, '[1-7]')
+        // ...collapse runs of '#'
+            .replace(/#{2,}/g, (s) => `#{${s.length}}`)
         // ...replace # with what ever named group precedes it
             .replace(/#/g, (_, i, s) => '\\k' + s.slice(0, i).match(/(?:\?(<\w+>))/g).pop().slice(1))
 );
