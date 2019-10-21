99d92eaf9743c3f62b83d40b03efb4793dba5908
Matthew Gregory <m.gregory@celcat.com>
2019-10-21 17:19:20 +0100

    removed gui tests

--- apps/scorer/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'scorer',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/scorer',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js'
-  ]
-};
--- libs/common/src/lib/notation.spec.ts
+++ libs/common/src/lib/notation.spec.ts
@@ -1,10 +1,6 @@
-import * as utils from './tile-utils';
-import { checkForMahjong, calculateWaits } from './mahjong';
-import { OpenMeld, Wind } from '@riichi/common';
 import { handFromNotation } from './notation';
 
 describe('Notation', () => {
-    // beforeEach(() => cy.visit('/'));
     describe('should parse', () => {
         it('a simple hand', () => {
             const hand = handFromNotation('123456789m');
@@ -13,12 +9,9 @@ describe('Notation', () => {
         });
 
         it ('verbose tiles in closed form', () => {
-            // const hand
+            const hand = handFromNotation('1m 2m 3m 4m 5m 6m 7m 8m 9m');
+            expect(hand.concealedTiles.length).toBe(9);
+            expect(hand.melds.length).toBe(0);
         });
-
     });
-
-
-
-
 });
